9eb033b004ec25a6c2ccd03ee1feaa57d2935ac6
Hendrik Schroeter <h.schroeter@pm.me>
2021-10-29 16:36:11 +0200

    github: Clean up

--- .github/workflows/build_linux_wheel.sh
+++ .github/workflows/build_linux_wheel.sh
@@ -9,6 +9,5 @@ ${PYBIN}/pip install maturin
 ${PYBIN}/maturin build --release -m $MANIFEST -i ${PYBIN}/python
 
 for wheel in target/wheels/*.whl; do
-    ll ${wheel}
     auditwheel repair "${wheel}"
 done
