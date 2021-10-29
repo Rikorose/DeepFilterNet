d448caaca07709ccfa8dbbcddf816b2219147430
Hendrik Schroeter <h.schroeter@pm.me>
2021-10-29 13:19:23 +0200

    github: WIP

--- .github/workflows/build_linux_wheel.sh
+++ .github/workflows/build_linux_wheel.sh
@@ -3,10 +3,14 @@
 PYTHON_V=$1
 MANIFEST=$2
 PYTHON_TAG=$(echo $PYTHON_V | tr -d ".")
-PYBIN=$(ls /opt/python/cp$PYTHON_TAG*/bin)
+PYBIN="/opt/python/cp$PYTHON_TAG*/bin"
 
-$PYBIN/pip install maturin
-$PYBIN/maturin build --release -m $MANIFEST
+echo $PYBIN
+echo ${PYBIN}
+ls ${PYBIN}
+
+${PYBIN}/pip install maturin
+${PYBIN}/maturin build --release -m $MANIFEST
 
 for wheel in target/wheels/*.whl; do
     auditwheel repair "${wheel}"
