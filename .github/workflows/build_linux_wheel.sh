4bc174c3ec88ef5cd32c38fe7a52a1a102a7448d
Hendrik Schroeter <h.schroeter@pm.me>
2021-10-29 13:16:53 +0200

    github: Fix python bin dir

--- .github/workflows/build_linux_wheel.sh
+++ .github/workflows/build_linux_wheel.sh
@@ -3,7 +3,11 @@
 PYTHON_V=$1
 MANIFEST=$2
 PYTHON_TAG=$(echo $PYTHON_V | tr -d ".")
-PYBIN=$(ls /opt/python/cp$PYTHON_TAG*/python/bin)
+PYBIN=$(ls /opt/python/cp$PYTHON_TAG*/bin)
 
 $PYBIN/pip install maturin
 $PYBIN/maturin build --release -m $MANIFEST
+
+for wheel in target/wheels/*.whl; do
+    auditwheel repair "${wheel}"
+done
