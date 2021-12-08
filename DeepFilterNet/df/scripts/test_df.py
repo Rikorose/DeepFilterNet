babf15a7b203f9fc0fd8dfb0be83d32dd3596b68
Hendrik Schroeter <h.schroeter@pm.me>
2021-12-08 15:30:18 +0100

    df: Add more metrics when octave is not available

--- DeepFilterNet/df/scripts/test_df.py
+++ DeepFilterNet/df/scripts/test_df.py
@@ -3,12 +3,43 @@
 import os
 
 import numpy as np
+from icecream import install
 from loguru import logger
+
+install()
 
 import df
 from df.deepfilternet import ModelParams
 from df.enhance import enhance, init_df, load_audio
-from df.scripts.test_voicebank_demand import composite
+from df.scripts.test_voicebank_demand import composite, si_sdr_speechmetrics, stoi
+
+
+def try_eval_composite(clean, enhanced, sr):
+    logger.info("Computing composite metrics")
+    try:
+        c_enh = composite(clean.numpy(), enhanced.numpy(), sr)
+        logger.info(f"Got {c_enh}")
+    except OSError:
+        logger.warning("Octave not found. Skipping.")
+        return
+    assert np.isclose(
+        c_enh, [2.63813972, 3.85677449, 2.51349003, 3.22993828, -2.69618571]
+    ).all(), f"Metric output not close: {c_enh}"
+
+
+def eval_pystoi(clean, enhanced, sr):
+    logger.info("Computing STOI")
+    s = stoi(clean.squeeze(0), enhanced.squeeze(0), sr)
+    logger.info(f"Got {s}")
+    assert np.isclose([s], [0.9689226932773523])
+
+
+def eval_sdr(clean, enhanced):
+    logger.info("Computing SI-SDR")
+    s = si_sdr_speechmetrics(clean.numpy(), enhanced.numpy())
+    logger.info(f"Got {s}")
+    assert np.isclose([s], [18.878527879714966])
+
 
 if __name__ == "__main__":
     df_dir = os.path.abspath(os.path.join(os.path.dirname(df.__file__), os.pardir))
@@ -22,8 +53,6 @@ if __name__ == "__main__":
     )
     logger.info("Running model")
     enhanced = enhance(model, df_state, noisy, pad=True)
-    logger.info("Computing composite metrics")
-    c_enh = composite(clean.numpy(), enhanced.numpy(), sr)
-    assert np.isclose(
-        c_enh, [2.63813972, 3.85677449, 2.51349003, 3.22993828, -2.69618571]
-    ).all(), f"Metric output not close: {c_enh}"
+    try_eval_composite(clean, enhanced, sr)
+    eval_pystoi(clean, enhanced, sr)
+    eval_sdr(clean, enhanced)
