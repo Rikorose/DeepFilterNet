fd2e00ed6c753c6670bf39cbc9410aca53ed3e9e
Hendrik Schroeter <h.schroeter@pm.me>
2021-12-10 12:23:58 +0100

    build: Pubish crates first before pushing release tag

--- release.sh
+++ release.sh
@@ -54,16 +54,16 @@ fd "(pyproject)|(Cargo)" -t f -e toml -x bash -c "set_version {} $VERSION"
 cargo add --manifest-path ./pyDF/Cargo.toml deep_filter@"$VERSION" --features transforms
 cargo add --manifest-path ./pyDF-data/Cargo.toml --features dataset deep_filter@"$VERSION"
 
+(
+  cd libDF
+  cargo publish --allow-dirty
+)
+
 fd "(pyproject)|(Cargo)" -t f -e toml -X git add {}
 
 git commit -m "v$VERSION"
 git push
 git tag -f "v$VERSION"
 git push -f --tags
-
-(
-  cd libDF
-  cargo publish --allow-dirty
-)
 
 cargo update
