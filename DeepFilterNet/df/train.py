2ee92cdef0d6f3204da8faa38bfb9753fd821cd5
Hendrik Schroeter <h.schroeter@pm.me>
2021-12-16 15:01:05 +0100

    df: Disable gradient during evaluation

--- DeepFilterNet/df/train.py
+++ DeepFilterNet/df/train.py
@@ -239,12 +239,13 @@ def run_epoch(
         atten = batch.atten.to(dev, non_blocking=True)
         snrs = batch.snr.to(dev, non_blocking=True)
         with torch.autograd.set_detect_anomaly(detect_anomaly):
-            enh, m, lsnr, df_alpha = model.forward(
-                spec=as_real(noisy),
-                feat_erb=feat_erb,
-                feat_spec=feat_spec,
-                atten_lim=atten,
-            )
+            with torch.set_grad_enabled(is_train):
+                enh, m, lsnr, df_alpha = model.forward(
+                    spec=as_real(noisy),
+                    feat_erb=feat_erb,
+                    feat_spec=feat_spec,
+                    atten_lim=atten,
+                )
             try:
                 err = losses.forward(
                     clean,
@@ -393,7 +394,9 @@ def summary_write(
     )
     torchaudio.save(os.path.join(summary_dir, f"{split}_enh_snr{snr}.wav"), synthesis(enh[0]), p.sr)
     np.savetxt(
-        os.path.join(summary_dir, f"{split}_lsnr_snr{snr}.txt"), lsnr[0].detach().cpu().numpy()
+        os.path.join(summary_dir, f"{split}_lsnr_snr{snr}.txt"),
+        lsnr[0].detach().cpu().numpy(),
+        fmt="%.3f",
     )
     np.savetxt(
         os.path.join(summary_dir, f"{split}_df_alpha_snr{snr}.txt"),
