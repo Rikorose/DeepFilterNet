6958bb86da963db5b525f345ee9dee6e34bebd1e
Hendrik Schroeter <h.schroeter@pm.me>
2022-01-20 10:08:26 +0100

    df: Add script to sample from a hdf5 dataset

---
+++ DeepFilterNet/df/scripts/sample_from_hdf5.py
@@ -0,0 +1,52 @@
+import argparse
+import io
+
+import h5py
+import numpy as np
+import torchaudio as ta
+
+
+def load_vorbis(buffer: np.ndarray):
+    wav, _ = ta.load(io.BytesIO(buffer[...].tobytes()), format="vorbis")
+    return wav
+
+
+def load_flac(buffer: np.ndarray):
+    wav, _ = ta.load(io.BytesIO(buffer[...].tobytes()), format="flac")
+    return wav
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("hdf5_file")
+    parser.add_argument("--sr", type=int, default=None)
+    parser.add_argument("--out-dir", "-o", type=str, default="out")
+    parser.add_argument("--n-samples", "-n", type=int, default=1)
+    args = parser.parse_args()
+
+    with h5py.File(args.hdf5_file, "r", libver="latest", swmr=True) as h5f:
+        sr = h5f.attrs.get("sr", args.sr)
+        if sr is None:
+            print("sr not found.")
+            exit(1)
+        n_samples = args.n_samples
+        i = 0
+        codec = h5f.attrs.get("codec", "pcm")
+        for group in h5f.keys():
+            for name, sample in h5f[group].items():
+                if codec == "vorbis":
+                    sample = load_vorbis(sample)
+                elif codec == "flac":
+                    sample = load_flac(sample)
+                outname = f"{args.out_dir}/{name}"
+                if not outname.endswith(".wav"):
+                    outname += ".wav"
+                print(outname)
+                ta.save(outname, sample[:], sample_rate=sr)
+                i += 1
+                if n_samples > 0 and i >= n_samples:
+                    break
+
+
+if __name__ == "__main__":
+    main()
