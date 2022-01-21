94325a27f938448fbe487225ed67647796b19e3a
Hendrik Schroeter <h.schroeter@pm.me>
2022-01-21 10:36:59 +0100

    scripts: Improvements for DNS script

--- scripts/download_process_dns4.sh
+++ scripts/download_process_dns4.sh
@@ -8,21 +8,30 @@ OUT_DIR=${OUT_DIR:-out/}
 mkdir -p "$WORKING_DIR"
 mkdir -p "$OUT_DIR"
 
-echo "Downloading DNS4 script"
-wget -q \
-  https://raw.githubusercontent.com/microsoft/DNS-Challenge/master/download-dns-challenge-4.sh \
-  -O "$WORKING_DIR"/dns4_blob.sh
-
-# Delete all stuff starting from WORKING_DIR="./datasets_fullband"
-sed -i -e '/^OUTPUT_PATH*/,$d' "$WORKING_DIR"/dns4_blob.sh
+if ! [[ -f "$WORKING_DIR"/dns4_blob.sh ]]; then
+  echo "Downloading DNS4 script"
+  wget -q \
+    https://raw.githubusercontent.com/microsoft/DNS-Challenge/master/download-dns-challenge-4.sh \
+    -O "$WORKING_DIR"/dns4_blob.sh
+  # Delete all stuff starting from WORKING_DIR="./datasets_fullband"
+  sed -i -e '/^OUTPUT_PATH*/,$d' "$WORKING_DIR"/dns4_blob.sh
+fi
 
 . "$WORKING_DIR"/dns4_blob.sh --source-only
 
 for BLOB in "${BLOB_NAMES[@]}"; do
     URL="$AZURE_URL/$BLOB"
     echo "Download: $BLOB"
-    curl -C - "$URL" | tar -C "$WORKING_DIR" -f - -x -j
-    echo "Download finished"
+    mkdir -p $(basename "$WORKING_DIR/$BLOB")
+    if command -v aria2c &> /dev/null; then
+      aria2c --dir "$WORKING_DIR" --log-level=warn "$URL"
+      BLOB=$(basename $BLOB) # aria2c removes any folders
+    else
+      wget --continue "$URL" -O "$WORKING_DIR/$BLOB"
+    fi
+    echo "Download finished. Extracting..."
+    tar -C "$WORKING_DIR" -f "$WORKING_DIR/$BLOB" -x -j
+    echo "extraction finished"
 
     if [[ $BLOB == *"noise"* ]]; then
       CODEC="vorbis"
@@ -39,10 +48,23 @@ for BLOB in "${BLOB_NAMES[@]}"; do
     else
       SPLIT="TRAIN"
     fi
-    fd -I . -e wav "$WORKING_DIR"/datasets_fullband/ > "$WORKING_DIR"/files.txt
-    HDF5_NAME="$(basename "$(dirname "$(head -n 1 "$WORKING_DIR"/files.txt)")")_$SPLIT.hdf5"
-    echo "Processing hdf5 dataset $OUT_DIR/$HDF5_NAME"
-    python DeepFilterNet/df/scripts/prepare_data.py "$TYPE" "$WORKING_DIR"/files.txt "$OUT_DIR"/"$HDF5_NAME" --codec "$CODEC"
+    for d in "$WORKING_DIR"/datasets_fullband/*/*; do
+      fd -I . -e wav "$d" > "$WORKING_DIR"/files.txt
+      HDF5_NAME="$(basename $d)_$SPLIT.hdf5"
+      echo "Processing hdf5 dataset $OUT_DIR/$HDF5_NAME"
+      python DeepFilterNet/df/scripts/prepare_data.py "$TYPE" "$WORKING_DIR"/files.txt "$OUT_DIR"/"$HDF5_NAME" --codec "$CODEC"
+      rm "$WORKING_DIR"/files.txt
+      # Delete dataset name from blob list
+      BLOB_ID=${BLOB%.tar.bz2}  # Remove extension
+      BLOB_ID=${BLOB_ID##*.}  # Select part after last dot
+      # Sed: Start from begining, find first matching id, delete it, stop
+      echo sed -i -e '0,/*'"$BLOB_ID"'*/{/'"$BLOB_ID"'/d;}' "$WORKING_DIR"/dns4_blob.sh
+      sed -i '0,/*'"$BLOB_ID"'*/{/'"$BLOB_ID"'/d;}' "$WORKING_DIR"/dns4_blob.sh
+    done
+    # Cleanup
+    echo "Deleting $WORKING_DIR"/datasets_fullband/*
     rm -rf "$WORKING_DIR"/datasets_fullband/*
-    rm "$WORKING_DIR"/files.txt
+    echo "Deleting $WORKING_DIR/$BLOB"
+    rm "$WORKING_DIR/$BLOB"
+    break
 done
