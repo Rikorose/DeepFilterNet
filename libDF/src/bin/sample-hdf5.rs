fffb4880dfa5afafac910816421a17f409153d99
Hendrik Schroeter <h.schroeter@pm.me>
2022-02-07 12:24:00 +0100

    libdf: Clippy

--- libDF/src/bin/sample-hdf5.rs
+++ libDF/src/bin/sample-hdf5.rs
@@ -4,12 +4,10 @@ use anyhow::Result;
 use df::dataset::Hdf5Dataset;
 
 fn main() -> Result<()> {
-    let mut args = args();
-    dbg!(&args);
-    let p = args.nth(2).expect("HDF5 dataset path expected");
-    dbg!(&p);
-    let n = args.nth(3).unwrap_or("1".to_string()).parse::<usize>()?;
-    let ds = Hdf5Dataset::new(&p)?;
+    let args = args().collect::<Vec<String>>();
+    let p = args.get(1).expect("HDF5 dataset path expected");
+    let n = args.get(2).unwrap_or(&"1".to_owned()).parse::<usize>()?;
+    let ds = Hdf5Dataset::new(p)?;
     let k = "LJSpeech-1.1_wavs_LJ044-0172.wav";
     let data = ds.read("LJSpeech-1.1_wavs_LJ044-0172.wav")?;
     dbg!(k, data.shape());
