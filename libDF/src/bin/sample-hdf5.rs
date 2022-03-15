25287532995d3ba6275ab658587e40220da08cb3
Hendrik Schroeter <h.schroeter@pm.me>
2022-03-15 13:20:59 +0100

    libdf: Fix bin

--- libDF/src/bin/sample-hdf5.rs
+++ libDF/src/bin/sample-hdf5.rs
@@ -8,6 +8,7 @@ use rand::seq::SliceRandom;
 fn main() -> Result<()> {
     let args = args().collect::<Vec<String>>();
     let p = args.get(1).expect("HDF5 dataset path expected");
+    let ds = Hdf5Dataset::new(p)?;
     let k = match args.get(2) {
         Some(k) => k.to_string(),
         None => ds.keys()?.choose(&mut rand::thread_rng()).unwrap().to_string(),
@@ -15,7 +16,7 @@ fn main() -> Result<()> {
     let data = ds.read(&k)?;
     dbg!(&k, data.shape());
     let name = "out/".to_owned() + &k;
-    println!("Sampled {}", k);
+    println!("Sampled {:?}", k);
     write_wav_arr2(&name, data.view(), ds.sr.unwrap() as u32).unwrap();
     Ok(())
 }
