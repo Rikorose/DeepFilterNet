c0d81c47031452e8404eb61625a12adaaae70452
Hendrik Schroeter <h.schroeter@pm.me>
2022-03-15 12:50:53 +0100

    libdf: Save sampled audio as wav

--- libDF/src/bin/sample-hdf5.rs
+++ libDF/src/bin/sample-hdf5.rs
@@ -2,18 +2,20 @@ use std::env::args;
 
 use anyhow::Result;
 use df::dataset::Hdf5Dataset;
+use df::wav_utils::write_wav_arr2;
+use rand::seq::SliceRandom;
 
 fn main() -> Result<()> {
     let args = args().collect::<Vec<String>>();
     let p = args.get(1).expect("HDF5 dataset path expected");
-    let n = args.get(2).unwrap_or(&"1".to_owned()).parse::<usize>()?;
-    let ds = Hdf5Dataset::new(p)?;
-    let k = "LJSpeech-1.1_wavs_LJ044-0172.wav";
-    let data = ds.read("LJSpeech-1.1_wavs_LJ044-0172.wav")?;
-    dbg!(k, data.shape());
-    for (k, _i) in ds.keys()?.iter().zip(0..n) {
-        let data = ds.read(k)?;
-        dbg!(k, data.shape());
-    }
+    let k = match args.get(2) {
+        Some(k) => k.to_string(),
+        None => ds.keys()?.choose(&mut rand::thread_rng()).unwrap().to_string(),
+    };
+    let data = ds.read(&k)?;
+    dbg!(&k, data.shape());
+    let name = "out/".to_owned() + &k;
+    println!("Sampled {}", k);
+    write_wav_arr2(&name, data.view(), ds.sr.unwrap() as u32).unwrap();
     Ok(())
 }
