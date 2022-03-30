ecb57cbc9410c67c3a86fecab6242c2ad5d3d7e0
Hendrik Schroeter <h.schroeter@pm.me>
2022-03-30 14:09:56 +0200

    df: Add model as key for host specific batch sizes

--- scripts/set_batch_size.py
+++ scripts/set_batch_size.py
@@ -6,7 +6,7 @@ from typing import List, Tuple
 
 
 def update_batch_size(
-    host: str,
+    host_key: str,
     config_parser: ConfigParser,
     host_bs_parser: ConfigParser,
     config_key: str,
@@ -15,7 +15,7 @@ def update_batch_size(
     current_bs = None
     if config_parser.has_section("train"):
         current_bs = config_parser.get("train", config_key, fallback=None)
-    batchconfig_bs = host_bs_parser.get(host, batchconfig_key, fallback=None)
+    batchconfig_bs = host_bs_parser.get(host_key, batchconfig_key, fallback=None)
     config_changed = False
     host_bs_changed = False
     if batchconfig_bs is not None:
@@ -30,16 +30,17 @@ def update_batch_size(
             config_parser.set("train", config_key, batchconfig_bs)
             config_changed = True
     elif current_bs is not None:
-        # No host specific batch config found. Store current batch size for current host
+        # No host_key specific batch config found. Store current batch size for current host
         print(
             f"Host specific {batchconfig_key} not found for host {host}. "
             "Updating host batch size config."
         )
-        if not host_bs_parser.has_section(host):
-            host_bs_parser.add_section(host)
-        host_bs_parser.set(host, batchconfig_key, current_bs)
+        if not host_bs_parser.has_section(host_key):
+            host_bs_parser.add_section(host_key)
+        host_bs_parser.set(host_key, batchconfig_key, current_bs)
         host_bs_changed = True
     return config_changed, host_bs_changed
+
 
 def cast_bool(value):
     value = str(value).lower()
@@ -48,7 +49,8 @@ def cast_bool(value):
     elif value in {"false", "no", "n", "off", "0"}:
         return False  # type: ignore
 
-def main(config_path, host_bs_config, host=None):
+
+def main(config_path, host_bs_config, host_key=None):
     """Sets host specific batch sizes in the specified config."""
     if not os.path.isfile(config_path):
         print(f"Config not found at path {config_path}.")
@@ -57,8 +59,8 @@ def main(config_path, host_bs_config, host=None):
         print(f"Host specific batch size config not found at path {host_bs_config}.")
         print("Creating default.")
         open(host_bs_config, "w").close()
-    if host is None:
-        host = gethostname()
+    if host_key is None:
+        host_key = gethostname()
     config_parser = ConfigParser()
     host_bs_parser = ConfigParser()
     with open(config_path, "r") as f:
@@ -68,18 +70,22 @@ def main(config_path, host_bs_config, host=None):
     # For eval, don't distinguish between autocasting
     changed: List[Tuple[bool, bool]] = []
     changed.append(
-        update_batch_size(host, config_parser, host_bs_parser, "batch_size_eval", "batch_size_eval")
+        update_batch_size(
+            host_key, config_parser, host_bs_parser, "batch_size_eval", "batch_size_eval"
+        )
     )
     autocast_enabled = cast_bool(config_parser.get("train", "train_autocast", fallback=False))
     if autocast_enabled:
         changed.append(
             update_batch_size(
-                host, config_parser, host_bs_parser, "batch_size", "batch_size_autocast_train"
+                host_key, config_parser, host_bs_parser, "batch_size", "batch_size_autocast_train"
             )
         )
     else:
         changed.append(
-            update_batch_size(host, config_parser, host_bs_parser, "batch_size", "batch_size_train")
+            update_batch_size(
+                host_key, config_parser, host_bs_parser, "batch_size", "batch_size_train"
+            )
         )
     # check whether the training config was changed
     if any(c[0] for c in changed):
