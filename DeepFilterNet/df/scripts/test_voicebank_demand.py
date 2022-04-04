22bd848864fb1d50a060bc4bf064b0273dfb4694
Hendrik Schroeter <h.schroeter@pm.me>
2022-04-04 16:16:54 +0200

    df: Add DNS 2020 test script

--- DeepFilterNet/df/scripts/test_voicebank_demand.py
+++ DeepFilterNet/df/scripts/test_voicebank_demand.py
@@ -1,16 +1,11 @@
 import glob
 import os
-from typing import List
 
-import torch
-import torch.multiprocessing as mp
 from loguru import logger
 
-from df.enhance import df_features, init_df, load_audio, save_audio, setup_df_argument_parser
-from df.evaluation_utils import HAS_OCTAVE, CompositeMetric, SiSDRMetric, StoiMetric, tqdm
+from df.enhance import init_df, save_audio, setup_df_argument_parser
+from df.evaluation_utils import HAS_OCTAVE, evaluation_loop
 from df.model import ModelParams
-from df.modules import get_device
-from df.utils import as_complex
 
 
 def main(args):
@@ -29,43 +24,22 @@ def main(args):
     noisy_dir = os.path.join(args.dataset_dir, "noisy_testset_wav")
     clean_dir = os.path.join(args.dataset_dir, "clean_testset_wav")
     assert os.path.isdir(noisy_dir) and os.path.isdir(clean_dir)
-    with mp.Pool(processes=args.metric_workers) as pool:
-        metrics: List[Metric] = [StoiMetric(sr, pool), SiSDRMetric(pool), CompositeMetric(sr, pool)]  # type: ignore
-        noisy_files = glob.glob(noisy_dir + "/*wav")
-        clean_files = glob.glob(clean_dir + "/*wav")
-        for noisyfn, cleanfn in tqdm(zip(noisy_files, clean_files), total=len(noisy_files)):
-            noisy, _ = load_audio(noisyfn, sr)
-            clean, _ = load_audio(cleanfn, sr)
-            enh = enhance(model, df_state, noisy)[0]
-            clean = df_state.synthesis(df_state.analysis(clean.numpy()))[0]
-            noisy = df_state.synthesis(df_state.analysis(noisy.numpy()))[0]
-            for m in metrics:
-                m.add(clean=clean, enhanced=enh, noisy=noisy)
-            enh = torch.as_tensor(enh).to(torch.float32).view(1, -1)
-            if args.output_dir is not None:
-                save_audio(
-                    os.path.basename(cleanfn),
-                    enh,
-                    sr,
-                    output_dir=args.output_dir,
-                    suffix=f"{suffix}",
-                )
-        del model, noisy, clean, enh
-        torch.cuda.empty_cache()
-        logger.info("Waiting for metrics computation completion. This could take a few minutes.")
-        for m in metrics:
-            for k, v in m.mean().items():
-                logger.info(f"{k}: {v}")
+    clean_files = glob.glob(clean_dir + "/*.wav")
+    noisy_files = glob.glob(noisy_dir + "/*.wav")
 
+    def save_audio_callback(cleanfn: str, enh):
+        save_audio(os.path.basename(cleanfn), enh, sr, output_dir=args.output_dir, suffix=suffix)
 
-@torch.no_grad()
-def enhance(model, df_state, audio):
-    model.eval()
-    if hasattr(model, "reset_h0"):
-        model.reset_h0(batch_size=1, device=get_device())
-    spec, erb_feat, spec_feat = df_features(audio, df_state, get_device())
-    spec = model(spec, erb_feat, spec_feat)[0]
-    return df_state.synthesis(as_complex(spec.squeeze(0)).cpu().numpy())
+    metrics = evaluation_loop(
+        df_state,
+        model,
+        clean_files,
+        noisy_files,
+        n_workers=args.metric_workers,
+        save_audio_callback=save_audio_callback if args.output_dir is not None else None,
+    )
+    for k, v in metrics.items():
+        logger.info(f"{k}: {v}")
 
 
 if __name__ == "__main__":
@@ -77,6 +51,7 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--metric-workers",
+        "-w",
         type=int,
         default=4,
         help="Number of worker processes for metric calculation.",
