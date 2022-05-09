031bb14b84e53fe5df2ec2236a21d698c5c6524b
Hendrik Schroeter <h.schroeter@pm.me>
2022-05-09 22:53:38 +0200

    df: Fix default config value

--- DeepFilterNet/df/deepfilternet2.py
+++ DeepFilterNet/df/deepfilternet2.py
@@ -70,7 +70,7 @@ class ModelParams(DfParams):
             "GROUP_SHUFFLE", cast=bool, default=True, section=self.section
         )
         self.dfop_method: str = config(
-            "DFOP_METHOD", cast=str, default="real_unfold", section=self.section
+            "DFOP_METHOD", cast=str, default="df", section=self.section
         )
         self.mask_pf: bool = config("MASK_PF", cast=bool, default=False, section=self.section)
 
