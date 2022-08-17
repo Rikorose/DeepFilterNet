4eef967a4b448af12e4fa6bdf0070730e3f2de8a
Hendrik Schroeter <h.schroeter@pm.me>
2022-08-17 08:19:45 +0200

    libdf: Misc error handling

--- libDF/src/dataset.rs
+++ libDF/src/dataset.rs
@@ -1849,9 +1849,9 @@ fn mix_audio_signal(
     noise *= mix_f(clean_out.view(), noise.view(), snr_db);
     let mut mixture = clean_mix + &noise;
     // Guard against clipping
-    let max = &([&clean_out, &noise, &mixture].iter().map(|x| find_max_abs(x.iter())))
-        .collect::<std::result::Result<Vec<f32>, crate::transforms::TransformError>>()?;
-    let max = find_max(max)?;
+    let max = &([&clean_out, &noise, &mixture].iter().map(|x| find_max_abs(x.iter()).unwrap()))
+        .collect::<Vec<f32>>();
+    let max = find_max(max).expect("NaN");
     if (max - 1.) > 1e-10 {
         let f = 1. / (max + 1e-10);
         clean_out *= f;
