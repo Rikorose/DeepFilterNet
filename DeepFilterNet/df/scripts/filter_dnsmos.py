dc7ce6e43ad10527ba647d269063f09e63add26e
Hendrik Schroeter <h.schroeter@pm.me>
2022-08-24 14:33:46 +0200

    df: Don't trim in filter_dnsmos

--- DeepFilterNet/df/scripts/filter_dnsmos.py
+++ DeepFilterNet/df/scripts/filter_dnsmos.py
@@ -12,7 +12,7 @@ import torchaudio as ta
 from icecream import ic
 from torch import Tensor
 
-import df.scripts.dnsmos_v2 as dnsmos
+import df.scripts.dnsmos as dnsmos
 from df.io import resample
 from df.scripts.prepare_data import encode
 
@@ -35,8 +35,15 @@ def to_int16(audio: Tensor) -> Tensor:
 
 
 def main(args):
-    onnx = dnsmos.download_onnx_model()
+    onnx_sig, onnx_bak_ovr = dnsmos.download_onnx_models()
+    if args.mos is not None:
+        assert len(args.mos)==3
+    t = args.mos
+    print(t)
+
     with h5py.File(args.hdf5_input, "r") as h5_read, h5py.File(args.hdf5_filtered, "a") as h5_write:
+        print(f"Opened datatset {args.hdf5_input}")
+
         # Copy attributes
         for (k, v) in h5_read.attrs.items():
             h5_write.attrs[k] = v
@@ -59,44 +66,25 @@ def main(args):
                     audio = torch.from_numpy(encoded)
                 else:
                     audio = load_encoded(encoded, codec=codec)
-                print(f"{key}, ", end="", flush=True)
+                print(f"{key} ...", end="", flush=True)
                 assert audio.dim() <= 2
                 # For now, only single channel is supported
                 assert audio.dim() == 1 or audio.shape[0] == 1
 
                 audio = to_f32(audio)
-                resampled = resample(audio, sr, dnsmos.SR)
-                # Check if we can trim up to first and last 3 seconds:
-                trim_start, trim_end = 0, None
-                for n in range(3, 0, -1):
-                    if trim_start == 0:
-                        (sig, _, ovr) = dnsmos.dnsmos_local(resampled[: n * dnsmos.SR], onnx)
-                        if sig < 2.5 or ovr < 2.8:
-                            trim_start = n * dnsmos.SR
-                    if trim_end is None:
-                        (sig, _, ovr) = dnsmos.dnsmos_local(resampled[-n * dnsmos.SR :], onnx)
-                        if sig < 2.5 or ovr < 2.8:
-                            trim_end = -1 * dnsmos.SR
-                # (sig, bak, ovr) = dnsmos.dnsmos_local(resampled, onnx)
-                # print(f"got sig: {sig:.2f}, bak: {bak:.2f}, ovr: {ovr:.2f} ... ", end="")
-                (sig, bak, ovr) = dnsmos.dnsmos_local(resampled[..., trim_start:trim_end], onnx)
-                print(f"got sig: {sig:.2f}, bak: {bak:.2f}, ovr: {ovr:.2f} ... ", end="")
-                if sig > 3.5 and bak > 4.0 and ovr > 3.7:
+                resampled = resample(audio.squeeze(), sr, dnsmos.SR)
+                (sig, bak, ovr) = dnsmos.dnsmos_local(resampled, onnx_sig, onnx_bak_ovr)
+                print(f" sig: {sig:.2f}, bak: {bak:.2f}, ovr: {ovr:.2f} ... ", end="")
+                if sig > t[0] and bak > t[1] and ovr > t[2]:
                     print("copying.")
-                    trim_start = trim_start // dnsmos.SR * sr
-                    trim_end = trim_end // dnsmos.SR * sr if trim_end is not None else None
-                    if trim_start>0 or trim_end is not None:
-                        print("Triming audio ({list(audio.shape)}): {trim_start}:{trim_end}")
-                    if trim_start == 0 and trim_end is None:
+                    if codec == codec_write:
                         data = encoded
                     else:
                         if codec == "pcm" and ds.dtype == "int16":
                             audio = to_int16(audio)
                         if audio.dim() == 1:
                             audio = audio.unsqueeze(0)
-                        data = encode(
-                            audio[..., trim_start:trim_end], sr, codec_write, compression=8
-                        )
+                        data = encode(audio, sr, codec_write, compression=8)
                     if key in group_write:
                         del group_write[key]
                     ds_write = group_write.create_dataset(
@@ -112,6 +100,7 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("hdf5_input", type=str)
+    parser.add_argument("--mos", type=float, default=None, nargs=3, help="Minimum values of SIG, BAK and OVR MOS values. Defaults to [4.2, 4.5, 4.0]")
     parser.add_argument("--hdf5_filtered", type=str, default=None)
     parser.add_argument("--codec", type=str, default=None)
     args = parser.parse_args()
@@ -119,6 +108,5 @@ if __name__ == "__main__":
     assert args.hdf5_input.endswith(".hdf5")
     if args.hdf5_filtered is None:
         args.hdf5_filtered = os.path.splitext(args.hdf5_input)[0] + "_filtered.hdf5"
-        ic(args.hdf5_filtered)
     assert args.hdf5_filtered.endswith(".hdf5")
     main(args)
