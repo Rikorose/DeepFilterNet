c5b5b89a6d7e543b1f335d4dc9a8c3e49ff62473
Hendrik Schroeter <Rikorose@users.noreply.github.com>
2022-09-14 13:45:53 +0200

    df: Better default model handling

--- DeepFilterNet/df/model.py
+++ DeepFilterNet/df/model.py
@@ -8,7 +8,7 @@ from df.config import DfParams, config
 
 class ModelParams(DfParams):
     def __init__(self):
-        self.__model = config("MODEL", default="deepfilternet", section="train")
+        self.__model = config("MODEL", default="deepfilternet3", section="train")
         self.__params = getattr(import_module("df." + self.__model), "ModelParams")()
 
     def __getattr__(self, attr: str):
@@ -17,7 +17,7 @@ class ModelParams(DfParams):
 
 def init_model(*args, **kwargs):
     """Initialize the model specified in the config."""
-    model = config("MODEL", default="deepfilternet", section="train")
+    model = config("MODEL", default="deepfilternet3", section="train")
     logger.info(f"Initializing model `{model}`")
     model = getattr(import_module("df." + model), "init_model")(*args, **kwargs)
     model.to(memory_format=torch.channels_last)
