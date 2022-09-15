b68a79d96d488c0d7d88a799a3789f90b34c43b4
Hendrik Schroeter <Rikorose@users.noreply.github.com>
2022-09-15 15:14:06 +0200

    df: Add print model script

---
+++ DeepFilterNet/df/scripts/print_model.py
@@ -0,0 +1,16 @@
+import sys
+import os
+
+from df.enhance import init_df
+
+
+def main():
+    dir = sys.argv[1] if len(sys.argv) > 1 else None
+    if dir is not None:
+        assert os.path.isdir(dir), f"Model base directory not found: {dir}"
+    model, _, _ = init_df(dir, log_level="ERROR", config_allow_defaults=True)
+    print(model)
+
+
+if __name__ == "__main__":
+    main()
