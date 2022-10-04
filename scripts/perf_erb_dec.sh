83900754f676bc39171173b4a95cd233f914cf68
Hendrik Schroeter <Rikorose@users.noreply.github.com>
2022-10-04 13:31:56 +0200

    libdf: Update tract to 0.18.0

--- scripts/perf_erb_dec.sh
+++ scripts/perf_erb_dec.sh
@@ -14,10 +14,11 @@ if ! [ -f "$1" ]; then
     exit 2
 fi
 
-tract --allow-random-input -v -O --partial --pulse 1 \
+tract -v -O --partial --pulse 1 \
   -i 1,S,256,f32 \
   -i 1,64,S,8,f32 \
   -i 1,64,S,8,f32 \
   -i 1,64,S,16,f32 \
   -i 1,64,S,32,f32 \
-  "$1" dump --profile --cost
+  --onnx-ignore-output-shapes \
+  "$1" dump --allow-random-input --profile --cost
