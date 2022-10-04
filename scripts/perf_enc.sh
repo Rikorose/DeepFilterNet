d192bffc15cb1ae253c075b572ce4c49e496ae12
Hendrik Schroeter <Rikorose@users.noreply.github.com>
2022-10-04 13:31:56 +0200

    libdf: Update tract to 0.18.0

--- scripts/perf_enc.sh
+++ scripts/perf_enc.sh
@@ -14,4 +14,6 @@ if ! [ -f "$1" ]; then
     exit 2
 fi
 
-tract --allow-random-input -v -O --partial --pulse 1 -i 1,1,S,32,f32 -i 1,2,S,96,f32 "$1" dump --profile --cost
+tract -v -O --partial --pulse 1 -i 1,1,S,32,f32 -i 1,2,S,96,f32 \
+  --onnx-ignore-output-shapes \
+  "$1" dump --allow-random-input --profile --cost
