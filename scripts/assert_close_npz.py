5ee008829c60f2eacb55a12846086c7130efcfa8
Hendrik Schroeter <Rikorose@users.noreply.github.com>
2022-10-12 19:34:11 +0200

    scripts: Test tract model with pulse

--- scripts/assert_close_npz.py
+++ scripts/assert_close_npz.py
@@ -1,24 +1,36 @@
 #!/bin/env python3
-
 import os
 import sys
 
+from icecream import ic
 import numpy as np
 
-ATOL=1e-4
-RTOL=1e-5
+ATOL = 1e-5
+RTOL = 1e-7
+
 
 def main():
-    usage = f"{sys.argv[0]} <a.npz> <b.npz> <input-name>"
-    assert len(sys.argv) == 4, usage
+    usage = f"{sys.argv[0]} <a.npz> <b.npz> <input-name> <concat-axis-pulse>"
+    assert len(sys.argv) in (4, 5), usage
     a = sys.argv[1]
     b = sys.argv[2]
     name = sys.argv[3]
     assert os.path.isfile(a), usage
     assert os.path.isfile(b), usage
-    a = np.load(a)
-    b = np.load(b)
-    np.testing.assert_allclose(a[name], b[name], atol=ATOL, rtol=RTOL)
+    npz_a = np.load(a)
+    npz_b = np.load(b)
+    # b is ref
+    assert name in npz_b, f"{name} not found in {b} (avail: {npz_b.files})"
+    ref = npz_b[name]
+    ic(name, ref.shape)
+    if name not in npz_a:
+        names = [f for f in npz_a.files if name in f]
+        actual = [npz_a[n] for n in names]
+        actual = np.concatenate(actual, axis=int(sys.argv[4]))
+        ic(actual.shape)
+    else:
+        actual = npz_a[name]
+    np.testing.assert_allclose(actual, ref, atol=ATOL, rtol=RTOL)
 
 
 if __name__ == "__main__":
--- scripts/run_enc.sh
+++ scripts/run_enc.sh
@@ -5,13 +5,13 @@ set -e
 USAGE="$0 <export-dir>"
 
 if [ "$#" -ne 1 ]; then
-    echo "$USAGE"
-    exit 1
+  echo "$USAGE"
+  exit 1
 fi
 
 if ! [ -d "$1" ]; then
-    echo "$USAGE"
-    exit 2
+  echo "$USAGE"
+  exit 2
 fi
 
 tract -v -O \
@@ -33,7 +33,6 @@ tract -v -O \
 # Filter encoder output for erb decoder usage (i.e. remove c0)
 ./scripts/split_npz.py "$1"/enc_output_tract.npz "$1"/erb_dec_input_tract.npz emb e0 e1 e2 e3
 
-# Why does this not work?
 tract -v -O \
   --onnx-ignore-output-shapes \
   "$1"/erb_dec.onnx run \
@@ -43,3 +42,34 @@ tract -v -O \
   --save-outputs "$1"/erb_dec_output_tract.npz
 
 ./scripts/assert_close_npz.py "$1"/erb_dec_output_tract.npz "$1"/erb_dec_output.npz m
+
+echo "Now with pulse:"
+echo
+
+tract -v \
+  "$1"/enc.onnx \
+  --onnx-ignore-output-shapes \
+  -i 1,1,S,32,f32 -i 1,2,S,96,f32 \
+  --pulse 1 run \
+  --input-from-bundle "$1"/enc_input.npz \
+  --assert-output-bundle "$1"/enc_output.npz \
+  --save-outputs "$1"/enc_output_tract.npz
+
+./scripts/assert_close_npz.py "$1"/enc_output_tract.npz "$1"/enc_output.npz lsnr 1
+./scripts/assert_close_npz.py "$1"/enc_output_tract.npz "$1"/enc_output.npz e0 2
+./scripts/assert_close_npz.py "$1"/enc_output_tract.npz "$1"/enc_output.npz e1 2
+./scripts/assert_close_npz.py "$1"/enc_output_tract.npz "$1"/enc_output.npz e2 2
+./scripts/assert_close_npz.py "$1"/enc_output_tract.npz "$1"/enc_output.npz e3 2
+./scripts/assert_close_npz.py "$1"/enc_output_tract.npz "$1"/enc_output.npz emb 1
+./scripts/assert_close_npz.py "$1"/enc_output_tract.npz "$1"/enc_output.npz c0 2
+
+tract -v \
+  "$1"/erb_dec.onnx \
+  --onnx-ignore-output-shapes \
+  -i 1,S,256,f32 -i 1,64,S,8,f32 -i 1,64,S,8,f32 -i 1,64,S,16,f32 -i 1,64,S,32,f32 \
+  --pulse 1 run \
+  --input-from-bundle "$1"/erb_dec_input_tract.npz \
+  --assert-output-bundle "$1"/erb_dec_output.npz \
+  --save-outputs "$1"/erb_dec_output_tract.npz
+
+./scripts/assert_close_npz.py "$1"/erb_dec_output_tract.npz "$1"/erb_dec_output.npz m 2
