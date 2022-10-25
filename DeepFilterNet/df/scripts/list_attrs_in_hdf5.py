4c86c8abdc00553f532cf7f6f3df3de1b1d345c1
Hendrik Schroeter <h.schroeter@pm.me>
2022-10-25 10:10:34 +0200

    df: Also list hdf5 keys

--- DeepFilterNet/df/scripts/list_attrs_in_hdf5.py
+++ DeepFilterNet/df/scripts/list_attrs_in_hdf5.py
@@ -3,11 +3,16 @@ import sys
 
 import h5py
 
-assert len(sys.argv) == 2
+assert len(sys.argv) in (2, 3)
 assert os.path.exists(sys.argv[1]), sys.argv[1]
+
+LIST_KEYS = len(sys.argv) == 3 and sys.argv[2] == "--keys"
 
 with h5py.File(sys.argv[1], "r", libver="latest", swmr=True) as h5f:
     for n, k in h5f.attrs.items():
         print(f"Found attr {n} '{k}' in {sys.argv[1]}")
     for group, samples in h5f.items():
         print(f"Found {len(samples)} samples in {group}")
+        if LIST_KEYS:
+            for sample in samples:
+                print(sample)
