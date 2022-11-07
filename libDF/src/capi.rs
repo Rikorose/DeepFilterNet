8cb95eb47a0652190f5be133feb3acf4e64576d3
Hendrik Schroeter <Rikorose@users.noreply.github.com>
2022-11-07 14:01:43 +0100

    libdf: Add fn to adjust attenuation limit at runtime

--- libDF/src/capi.rs
+++ libDF/src/capi.rs
@@ -13,8 +13,6 @@ impl DFState {
     fn new(model_path: &str, channels: usize, atten_lim: f32) -> Self {
         let r_params =
             RuntimeParams::new(channels, false, atten_lim, -10., 30., 20., ReduceMask::MEAN);
-        // let df_params =
-        //     DfParams::from_bytes(include_bytes!("../../../models/DeepFilterNet2_onnx.tar.gz"))?;
         let df_params =
             DfParams::new(PathBuf::from(model_path)).expect("Could not load model from path");
         let m =
@@ -48,6 +46,16 @@ pub unsafe extern "C" fn df_create(
 pub unsafe extern "C" fn df_get_frame_length(st: *mut DFState) -> usize {
     let state = st.as_mut().expect("Invalid pointer");
     state.0.hop_size
+}
+
+/// Get DeepFilterNet frame size in samples.
+///
+/// Args:
+///     - lim_db: New attenuation limit in dB.
+#[no_mangle]
+pub unsafe extern "C" fn df_set_atten_lim(st: *mut DFState, lim_db: f32) {
+    let state = st.as_mut().expect("Invalid pointer");
+    state.0.set_atten_lim(lim_db).expect("Failed to set attenuation limit.")
 }
 
 /// Processes a chunk of samples.
