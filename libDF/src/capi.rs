e06050ac7dd779906b7ab7c304eaf8b89570252d
Hendrik Schroeter <h.schroeter@pm.me>
2023-02-14 09:24:18 +0100

    df/capi: Return null ptr if stage was not applied

--- libDF/src/capi.rs
+++ libDF/src/capi.rs
@@ -96,21 +96,25 @@ pub unsafe extern "C" fn df_process_frame(
 pub unsafe extern "C" fn df_process_frame_raw(
     st: *mut DFState,
     input: *mut c_float,
-    out_gains: *mut c_float,
-    out_coefs: *mut c_float,
+    out_gains_p: *mut c_float,
+    out_coefs_p: *mut c_float,
 ) -> c_float {
     let state = st.as_mut().expect("Invalid pointer");
     let input = ArrayView2::from_shape_ptr((1, state.0.n_freqs), input);
     state.0.set_spec_buffer(input).expect("Failed to set input spectrum");
     let (lsnr, gains, coefs) = state.0.process_raw().expect("Failed to process DF spectral frame");
-    let mut out_gains = ArrayViewMut2::from_shape_ptr((1, state.0.nb_erb), out_gains);
+    let mut out_gains = ArrayViewMut2::from_shape_ptr((1, state.0.nb_erb), out_gains_p);
     let mut out_coefs =
-        ArrayViewMut4::from_shape_ptr((1, state.0.df_order, state.0.nb_df, 2), out_coefs);
+        ArrayViewMut4::from_shape_ptr((1, state.0.df_order, state.0.nb_df, 2), out_coefs_p);
     if let Some(gains) = gains {
         out_gains.assign(&gains.to_array_view().unwrap());
+    } else {
+        *out_gains_p = *std::ptr::null_mut();
     }
     if let Some(coefs) = coefs {
         out_coefs.assign(&coefs.to_array_view().unwrap());
+    } else {
+        *out_coefs_p = *std::ptr::null_mut();
     }
     lsnr
 }
@@ -122,6 +126,7 @@ pub struct DynArray {
     length: c_uint,
 }
 
+/// Get size of DeepFilter coefficients
 pub unsafe extern "C" fn df_coef_size(st: *const DFState) -> DynArray {
     let state = st.as_ref().expect("Invalid pointer");
     let mut shape = vec![
@@ -137,6 +142,7 @@ pub unsafe extern "C" fn df_coef_size(st: *const DFState) -> DynArray {
     std::mem::forget(shape);
     ret
 }
+/// Get size ERB gains
 pub unsafe extern "C" fn df_gain_size(st: *const DFState) -> DynArray {
     let state = st.as_ref().expect("Invalid pointer");
     let mut shape = vec![state.0.ch as u32, state.0.nb_erb as u32];
