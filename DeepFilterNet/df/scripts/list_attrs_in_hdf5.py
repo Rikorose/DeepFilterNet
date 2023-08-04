3c9ce46a7f853bd4e91be106d1c7d14d9a2b09ff
Hendrik Schroeter <h.schroeter@pm.me>
2023-08-04 12:00:30 +0200

    df: Compute total hours over multiple datasets

--- DeepFilterNet/df/scripts/list_attrs_in_hdf5.py
+++ DeepFilterNet/df/scripts/list_attrs_in_hdf5.py
@@ -1,13 +1,15 @@
+import argparse
 import os
 import sys
 
 import h5py
 import torch
 
-assert len(sys.argv) in (2, 3)
-assert os.path.exists(sys.argv[1]), sys.argv[1]
-
-LIST_KEYS = len(sys.argv) == 3 and sys.argv[2] == "--keys"
+parser = argparse.ArgumentParser()
+parser.add_argument("hdf5", nargs="+")
+parser.add_argument("--keys", "-k", action="store_true")
+parser.add_argument("--hours", action="store_true")
+args = parser.parse_args()
 
 
 def load_encoded(buffer, codec: str):
@@ -20,23 +22,29 @@ def load_encoded(buffer, codec: str):
     return wav
 
 
-with h5py.File(sys.argv[1], "r", libver="latest", swmr=True) as h5f:
-    for n, k in h5f.attrs.items():
-        print(f"Found attr {n} '{k}' in {sys.argv[1]}")
+total_hours = 0
 
-    total_len = 0
-    for group, samples in h5f.items():
-        print(f"Found {len(samples)} samples in {group}")
-        codec = h5f.attrs.get("codec", "pcm")
-        sr = h5f.attrs["sr"]
-        if LIST_KEYS:
-            for n, sample in samples.items():
-                print(n)
-                if codec == "pcm":
-                    audio = torch.from_numpy(sample[...])
-                    if audio.dim() == 1:
-                        audio.unsqueeze_(0)
-                else:
-                    audio = load_encoded(sample, codec)
-                total_len += audio.shape[-1] / sr / 60 / 60
-        print("Total len [h]:", total_len)
+for f in args.hdf5:
+    assert os.path.isfile(f)
+    with h5py.File(f, "r", libver="latest", swmr=True) as h5f:
+        for n, k in h5f.attrs.items():
+            print(f"Found attr {n} '{k}' in {f}")
+
+        for group, samples in h5f.items():
+            print(f"Found {len(samples)} samples in {group}")
+            codec = h5f.attrs.get("codec", "pcm")
+            sr = h5f.attrs["sr"]
+            if args.keys or args.hours:
+                for n, sample in samples.items():
+                    if args.keys:
+                        print(n)
+                    if codec == "pcm":
+                        audio = torch.from_numpy(sample[...])
+                        if audio.dim() == 1:
+                            audio.unsqueeze_(0)
+                    else:
+                        audio = load_encoded(sample, codec)
+                    total_hours += audio.shape[-1] / sr / 60 / 60
+
+if args.hours:
+    print("Total len [h]:", total_hours)
